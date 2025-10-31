1c52766e5308180c88f11658d76670ded95b82e2
Madan Pokhrel <madhanpokhrel9@gmail.com>
2025-10-31 18:52:28 +0545

    user model added

---
+++ backend/src/app.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors({ origin: process.env.FRONTEND_URL })); 
+app.use(express.json())
+
+export default app;
---
+++ backend/src/server.ts
@@ -0,0 +1,17 @@
+import http from 'http';
+import app from './app';
+import connectDB from './config/db';
+
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+
+connectDB();
+
+const server = http.createServer(app);
+
+server.listen(PORT, () => {
+     console.log(`Server running on port ${PORT}`);
+})
